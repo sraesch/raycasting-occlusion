67e4a01e58534e2b887cd0ec2f953e6d2a61f449
Sascha Raesch <sascha.raesch@gmail.com>
2025-03-08 18:41:12 +0100

    :construction: added wip state of BVH

--- occ-raycasting/src/spatial/bvh.rs
+++ occ-raycasting/src/spatial/bvh.rs
@@ -1,4 +1,6 @@
-use crate::math::AABB;
+use crate::math::{aabb_ray, AABB};
+
+use super::{HierarchicalIndex, HierarchicalNode, RayIntersectionTest};
 
 /// Bounding Volume Hierarchy
 pub struct BVH {
@@ -7,6 +9,21 @@ pub struct BVH {
 
     /// The objects of the BVH.
     objects: Vec<usize>,
+}
+
+impl HierarchicalIndex for BVH {
+    type Volume = AABB;
+    type Node = Node;
+
+    #[inline]
+    fn nodes(&self) -> &[Self::Node] {
+        &self.nodes
+    }
+
+    #[inline]
+    fn object_indices(&self) -> &[usize] {
+        self.objects.as_slice()
+    }
 }
 
 pub struct Node {
@@ -18,6 +35,56 @@ pub struct Node {
 
     /// The range of the objects stored in the node.
     objects: std::ops::Range<u32>,
+}
+
+impl HierarchicalNode for Node {
+    type Volume = AABB;
+
+    #[inline]
+    fn children(&self) -> std::ops::Range<usize> {
+        if self.children == 0 {
+            0..0
+        } else {
+            (self.children as usize)..(self.children as usize + 2)
+        }
+    }
+
+    #[inline]
+    fn objects(&self) -> std::ops::Range<usize> {
+        self.objects.start as usize..self.objects.end as usize
+    }
+
+    #[inline]
+    fn bounding_volume(&self) -> &Self::Volume {
+        &self.volume
+    }
+
+    fn intersect_children(
+        &self,
+        ray: &crate::math::Ray,
+        children_indices: &mut [usize],
+        nodes: &[Self],
+        max_depth: Option<f32>,
+    ) -> usize {
+        let mut count = 0;
+
+        let mut f = [0f32; 2];
+        for (f, i) in f.iter_mut().zip(self.children()) {
+            if let Some(t) = nodes[i].bounding_volume().intersects_ray(ray, max_depth) {
+                children_indices[count] = i;
+                count += 1;
+
+                *f = t;
+            }
+        }
+
+        // Sort the children by distance to the ray origin.
+        if count == 2 && f[0] > f[1] {
+            children_indices.swap(0, 1);
+        }
+
+        count
+    }
 }
 
 pub struct Builder {
@@ -56,4 +123,10 @@ impl Builder {
 pub struct BVHOptions {
     pub max_depth: usize,
     pub max_objects_per_node: usize,
+}
+
+impl RayIntersectionTest for AABB {
+    fn intersects_ray(&self, ray: &crate::math::Ray, max_depth: Option<f32>) -> Option<f32> {
+        aabb_ray(self, ray, max_depth)
+    }
 }
